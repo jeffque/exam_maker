b65c2579534a78631720e6011c8d8dfec18cc770
Jefferson Quesado <jeff.quesado@gmail.com>
2014-10-30 00:48:03 -0300

    Adding comment
    Some day it will be blog like comment… some day…

--- repo/quest_viewer/admin.py
+++ repo/quest_viewer/admin.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 
 from quest_viewer.models import *
 
-class DisciplinesSubject__relationshipInline(admin.TabularInline):
+class DisciplinesSubjectRelationshipInline(admin.TabularInline):
 	model = DisciplinesSubject
 	extra = 1
 
 class DisciplineAdmin(admin.ModelAdmin):
-	inlines = (DisciplinesSubject__relationshipInline, )
+	inlines = (DisciplinesSubjectRelationshipInline, )
 
 class SubjectAdmin(admin.ModelAdmin):
-	inlines = (DisciplinesSubject__relationshipInline, )
+	inlines = (DisciplinesSubjectRelationshipInline, )
 	list_display = ('name', 'main_disciplines_list_comma_separated')
 
 admin.site.register(Discipline, DisciplineAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Institute)
 admin.site.register(Source)
+admin.site.register(Comment)
