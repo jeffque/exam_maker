62174971636774edc26e9abe39643fac0213a666
Jefferson Quesado <jeff.quesado@gmail.com>
2014-10-30 00:19:00 -0300

    Many many registry between discipline and subject

--- repo/quest_viewer/admin.py
+++ repo/quest_viewer/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 
 from quest_viewer.models import *
 
-admin.site.register(Discipline)
-admin.site.register(Subject)
+class DisciplinesSubject__relationshipInline(admin.TabularInline):
+	model = DisciplinesSubject
+	extra = 1
+
+class DisciplineAdmin(admin.ModelAdmin):
+	inlines = (DisciplinesSubject__relationshipInline, )
+
+class SubjectAdmin(admin.ModelAdmin):
+	inlines = (DisciplinesSubject__relationshipInline, )
+
+admin.site.register(Discipline, DisciplineAdmin)
+admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Institute)
 admin.site.register(Source)
-
