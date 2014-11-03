fa07d65a9b7b1bab18a21485d27bc6403af2d5c3
Jefferson Quesado <jeff.quesado@gmail.com>
2014-11-03 02:22:38 -0300

    Creating some initial views
    Views for the following were created:
    * discipline
    * subject
    * index (points to the list of disciplines)

--- repo/quest_viewer/urls.py
+++ repo/quest_viewer/urls.py
@@ -5,4 +5,5 @@ from quest_viewer import views
 urlpatterns = patterns('',
 		    url(r'^$', views.index, name='index'),
 		    url(r'^discipline/(?P<discipline_id>\d+)$', views.discipline, name='discipline'),
+		    url(r'^subject/(?P<subject_id>\d+)$', views.subject, name='subject'),
 		    )
--- repo/quest_viewer/views.py
+++ repo/quest_viewer/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from quest_viewer.models import *
 
 def index(request):
-	return HttpResponse('Test')
+	discipline_list = Discipline.objects.all()
+	context = {'discipline_list': discipline_list}
+	return render(request, 'quest_viewer/index.html', context)
 
 def discipline(request,discipline_id):
-	return HttpResponse('ID used for discipline: %s' % discipline_id)
+	discipline = Discipline.objects.get(id=discipline_id)
+	subject_list = discipline.subject_set.all()
+	context = {'subject_list': subject_list,
+			'discipline_id': discipline_id,
+			'discipline': discipline}
+	return render(request, 'quest_viewer/discipline.html', context)
+
+def subject(request,subject_id):
+	subject = Subject.objects.get(id=subject_id)
+	discipline_list = subject.disciplinessubject_set.all()
+	context = {'discipline_list': discipline_list,
+			'subject_id': subject_id,
+			'subject': subject}
+	return render(request, 'quest_viewer/subject.html', context)
