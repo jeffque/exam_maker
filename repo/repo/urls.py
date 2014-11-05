f38b80350113d470834ea82bb287873090cb9d2a
Jefferson Quesado <jeff.quesado@gmail.com>
2014-11-05 19:28:08 -0300

    Namespacing urls

--- repo/repo/urls.py
+++ repo/repo/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^v/', include('quest_viewer.urls')),
+    url(r'^v/', include('quest_viewer.urls', namespace='quest_viewer')),
 )
